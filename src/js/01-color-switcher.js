39c3915f6afa31065d407696e6f00be5dfe28d65
AgaP151 <piotrowska.agnieszka1987@gmail.com>
2023-09-28 15:40:01 +0200

    ninth homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
+
+let timerId;
+
+stopButtonEl.disabled = true;
+
+const handleOnStartClick = () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startButtonEl.disabled = true;
+    stopButtonEl.disabled = false;
+}
+
+const handleOnStopClick = () => {
+    startButtonEl.disabled = false;
+    clearInterval(timerId);
+    stopButtonEl.disabled = true;
+}
+
+startButtonEl.addEventListener('click', handleOnStartClick);
+stopButtonEl.addEventListener('click', handleOnStopClick);
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
